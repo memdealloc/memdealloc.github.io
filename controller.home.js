7028e8253eed82fbf3c5095905f9cd859aaca3f1
ToastoftheAges <andrewj2013@gmail.com>
2017-03-11 21:49:01 -0600

    Blessed

--- controller.home.js
+++ controller.home.js
@@ -882,7 +882,7 @@ app.controller('HomeCtrl', ['$scope', '$location', '$interval', 'DataService', f
     $scope.enemyHasStatus = function(index){
     	var status = $scope.enemyData[index][29][0];
     	//these are statuses that don't display an icon
-    	if(status != "None" && status != "Emerging" && status != "Marked" && status != "Defeated" && status != "Disconcerted") return true;
+    	if(status != "None" && status != "Emerging" && status != "Marked" && status != "Defeated" && status != "Disconcerted" && status != "Blessed") return true;
     	else return false;
     };
     
